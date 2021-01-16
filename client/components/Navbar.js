0a493893ba7a760cd4a50c86895fc53a9599d391
Tucker2015 <kevtucker@gmail.com>
2021-01-16 22:02:36 +0000

    - Edit more CSS

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 require('./NavBar.scss');
 
+
 export default class Navbar extends React.Component {
+
+
+
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary ">
                 <div className="container">
-                    <Link to={'/'} className={'navbar-brand'}>
+                    <Link to={'/'} className={'navbar-brand'} >
                         MixStream
                     </Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"/>
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ml-auto">
-                        
-                        <li className="nav-item float-right">
+
+                            <li className="nav-item float-right">
                                 <a className="nav-link" href="/">Home</a>
                             </li>
                             <li className="nav-item float-right">
-                                <Link className={'nav-link t-hover'} to={'/settings'} >
+                                <Link className={'nav-link'} to={'/settings'} >
                                     Go Live
                                 </Link>
                             </li>
-                            
+
                             <li className="nav-item float-right">
                                 <a className="nav-link" href="/Logout">Logout</a>
                             </li>
